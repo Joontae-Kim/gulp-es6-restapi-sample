01ccf602e1c34a441b92b99828681c6318297515
김준태 <bexellenting@gmail.com>
2018-03-03 19:04:33 +0900

    Update gulp task

--- backend/gulpfile.babel.js
+++ backend/gulpfile.babel.js
@@ -3,6 +3,44 @@
 import gulp from 'gulp';
 import gutil from 'gulp-util';
 
-gulp.task('default', () => {
+import uglify from 'gulp-uglify';
+import babel from 'gulp-babel';
+import Cache from 'gulp-file-cache';
+import nodemon from 'gulp-nodemon'
+import livereload from 'gulp-livereload';
+
+import pump from 'pump';
+import del from 'del';
+
+let cache = new Cache();
+
+// Definition Directory
+const DIR = {
+    SRC: 'src',
+    DEST: 'dist'
+};
+
+const SRC = {
+    JS: DIR.SRC + '/**/*.js'
+};
+
+const DEST = {
+    JS: DIR.DEST
+};
+
+gulp.task('clean', () => {
+    return del.sync([DIR.DEST]);
+});
+
+gulp.task('js', () => {
+  return gulp.src(SRC.JS)
+          .pipe(babel({
+              presets: ['es2015']
+          }))
+         .pipe(uglify())
+         .pipe(gulp.dest(DEST.JS));
+});
+
+gulp.task('default', ['clean', 'js'], () => {
     return gutil.log('Gulp is running');
 });
