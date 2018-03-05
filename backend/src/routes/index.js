119d751668cd659bb44bb699939d6c0d7db37ed8
김준태 <bexellenting@gmail.com>
2018-03-05 13:18:14 +0900

    Change sample routes js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -14,4 +14,8 @@ router.get('/read/:id', (req, res) => {
     res.send({msg:'You are reading article ' + req.params.id});
 });
 
+router.get('/post/:id', (req, res) => {
+    res.send({msg:'You are posting article ' + req.params.id});
+});
+
 module.exports = router;
